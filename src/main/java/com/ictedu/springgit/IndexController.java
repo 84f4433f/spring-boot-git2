aa96fbb52a19aaa592c8ad9cdf6163ea70a4c5b9
ChooInCheol <gg9465@naver.com>
2024-07-16 15:37:52 +0900

    루트 요청컨트롤러

--- src/main/java/com/ictedu/springgit/IndexController.java
+++ src/main/java/com/ictedu/springgit/IndexController.java
@@ -1,5 +1,13 @@
 package com.ictedu.springgit;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class IndexController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
